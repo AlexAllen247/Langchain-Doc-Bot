f4901359fe07b4b8f3db2c0987d0db5f2ab0a483
Alex Allen <99975765+AlexAllen247@users.noreply.github.com>
2023-06-29 13:05:44 +0200

    little change

--- app3.py
+++ app3.py
@@ -2,6 +2,7 @@ from langchain.chains.api.prompt import API_RESPONSE_PROMPT
 from langchain.chains import APIChain
 from langchain.prompts.prompt import PromptTemplate
 from langchain.llms import OpenAI
+from langchain.chains.api import open_meteo_docs
 from dotenv import load_dotenv
 import os
 
@@ -10,8 +11,6 @@ load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key)
-
-from langchain.chains.api import open_meteo_docs
 
 chain_new = APIChain.from_llm_and_api_docs(
     llm, open_meteo_docs.OPEN_METEO_DOCS, verbose=True
